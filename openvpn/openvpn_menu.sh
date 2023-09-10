f662f39e8f66a811f52c1ad69d3044c66e374009
0nyr <rascoussier.florian@gmail.com>
2023-09-10 23:59:29 +0200

    Added uni passau server to openvpn menu.

--- openvpn/openvpn_menu.sh
+++ openvpn/openvpn_menu.sh
@@ -4,13 +4,17 @@
 source ~/.config/openvpn/.env
 
 # Rofi menu
-OPTIONS="󰒄 Uni Passau VPN\n Cancel"
+OPTIONS="󰒄 Uni Passau VPN\n Uni Passau Server\n Cancel"
 CHOICE=$(echo -e $OPTIONS | rofi -dmenu -p "System")
 
 case $CHOICE in
     "󰒄 Uni Passau VPN")
         # Connect to Uni Passau VPN
         kitty -e bash -c "sudo openvpn --cd ~/.config/openvpn/ --config ~/.config/openvpn/client/stud-ext.ovpn --auth-nocache --auth-user-pass ~/.config/openvpn/client/pass.txt; echo -e \"Waiting 100s...\"; sleep 100"
+        ;;
+    " Uni Passau Server")
+        # Connect to Uni Passau Server
+        kitty -e bash -c "sshpass -p $UNI_PASSAU_SERVER_PASSWORD ssh -p $UNI_PASSAU_SERVER_PORT $UNI_PASSAU_USER@$UNI_PASSAU_SERVER; echo -e \"Waiting 100s...\"; sleep 100"
         ;;
     " Cancel")
         ;;
