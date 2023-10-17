4e5cb35e495edf3eea86d6c8f18bfa20a4795adc
0nyr <rascoussier.florian@gmail.com>
2023-10-17 14:52:29 +0200

    Added Drogon server.

--- openvpn/openvpn_menu.sh
+++ openvpn/openvpn_menu.sh
@@ -5,7 +5,7 @@ ENV_FILE=~/.config/openvpn/.env
 source $ENV_FILE
 
 # Rofi menu
-OPTIONS="󰒄 Uni Passau VPN\n󰒄 INSA Lyon VPN\n Uni Passau Server\n Cancel"
+OPTIONS="󰒄 Uni Passau VPN\n󰒄 INSA Lyon VPN\n Uni Passau Server - deathstar\n Uni Passau Server - drogon\n Cancel"
 CHOICE=$(echo -e $OPTIONS | rofi -dmenu -p "System")
 
 # NOTE: Don't forget to add new entry to $OPTIONS variable
@@ -18,9 +18,13 @@ case $CHOICE in
         # VPN INSA Lyon
         kitty -e bash -c "source $ENV_FILE; echo -e \"$INSA_LYON_PASSWORD\" | sudo openconnect --protocol anyconnect --user $INSA_LYON_LOGIN --authgroup $INSA_LYON_GROUP --server sslvpn.cisr.fr; echo -e \"Waiting 100s...\"; sleep 100"
         ;;
-    " Uni Passau Server")
+    " Uni Passau Server - deathstar")
         # Connect to Uni Passau Server
         kitty -e bash -c "export TERM=xterm-256color; sshpass -p $UNI_PASSAU_SERVER_PASSWORD ssh -t -p $UNI_PASSAU_SERVER_PORT $UNI_PASSAU_USER@$UNI_PASSAU_SERVER; echo -e \"Waiting 100s...\"; sleep 100"
+        ;;
+    " Uni Passau Server - drogon")
+        # Connect to Uni Passau Server
+        kitty -e bash -c "export TERM=xterm-256color; sshpass -p $UNI_PASSAU_SERVER_PASSWORD_DROGON ssh -t -p $UNI_PASSAU_SERVER_PORT_DROGON $UNI_PASSAU_USER_DROGON@$UNI_PASSAU_SERVER_DROGON; echo -e \"Waiting 100s...\"; sleep 100"
         ;;
     " Cancel")
         ;;
